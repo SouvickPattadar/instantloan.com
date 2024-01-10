71716c87e70374611047b108aefad69156d928dd
SouvickPattadar <71371526+SouvickPattadar@users.noreply.github.com>
2024-01-10 10:19:45 +0530

    Update custom.js

--- custom.js
+++ custom.js
@@ -18,6 +18,13 @@
 		$('html , body').animate({scrollTop: 0},1000);
 		return false;
 	});
+
+		// Menu click Navigation Collapse
+		$('.navbar-nav li a').on('click', function(){
+    if(!$( this ).hasClass('dropdown-toggle')){
+        $('.navbar-collapse').collapse('hide');
+    }
+});
 		
 		});
 	
